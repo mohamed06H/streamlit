eddd524ed41e75165ac9cbcdd4062389fa20c5e5
Mohamed Hamiche <mohamed.hamiche@mydatamodels.com>
2023-02-19 17:49:15 +0100

    initial commit

---
+++ titanic-classification-web-app/app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+# Header 
+st.write("""
+# Titanic Survival Prediction App
+This app predicts if you would survive to titanic or not.
+""")
+
+# Sidebar
+st.sidebar.header('User Input Features')
+
+
+def user_input_features():
+    embarked = st.sidebar.selectbox('Embarkation port', ('C', 'S', 'Q'))
+    sex = st.sidebar.selectbox('Sex', ('male', 'female'))
+    pclass = st.sidebar.selectbox('Passenger Class', (1, 2, 3))
+    age = st.sidebar.slider('Age', 0, 99, 25)
+    fare = st.sidebar.slider('Fare', 0, 600, 33)
+    data = {'embarked': embarked,
+            'sex': sex,
+            'pclass': pclass,
+            'age': age,
+            'fare': fare}
+
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+
+# Input data from sidebar
+input_df = user_input_features()
+
+# Input data in a subheader
+st.subheader('User Input features')
+st.write(input_df)
+
+# Load saved model
+model = pickle.load(open('model/titanic.pkl', 'rb'))
+
+# Make prediction
+prediction = model.predict(input_df)
+prediction = int(prediction[0])
+prediction_proba = model.predict_proba(input_df)
+
+# Predictions 
+st.subheader('Prediction')
+outcome = np.array(['Survived', 'Did not survived !'])
+st.write(outcome[prediction])
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
---
+++ titanic-classification-web-app/model/train.py
@@ -0,0 +1,44 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.datasets import fetch_openml
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
+import pickle
+
+# Load dataset
+X, y = fetch_openml("titanic", version=1, as_frame=True, return_X_y=True)
+
+# Define preprocessing pipelines
+numeric_features = ["age", "fare"]
+numeric_transformer = Pipeline(
+    steps=[("imputer", SimpleImputer(strategy="mean")), ("scaler", StandardScaler())]
+)
+
+categorical_features = ["embarked", "sex", "pclass"]
+categorical_transformer = OneHotEncoder(handle_unknown="ignore")
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", numeric_transformer, numeric_features),
+        ("cat", categorical_transformer, categorical_features),
+    ]
+)
+
+# Append classifier to preprocessing pipeline
+model = Pipeline(
+    steps=[("preprocessor", preprocessor), ("classifier", LogisticRegression())]
+)
+
+# Split data 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# train the model 
+model.fit(X_train, y_train)
+
+# Save the model
+pickle.dump(model, open('model/titanic.pkl', 'wb'))
+
