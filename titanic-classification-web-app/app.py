8674f9ac31abd0aecb4496ec0e50dc809f1e9379
Mohamed Hamiche <mohamed.hamiche@mydatamodels.com>
2023-08-17 10:58:04 +0200

    fix: loading pickle file

--- titanic-classification-web-app/app.py
+++ titanic-classification-web-app/app.py
@@ -39,10 +39,12 @@ st.subheader('User Input features')
 st.write(input_df)
 
 # Load saved model
-pickle_file_path = os.path.join(os.getcwd(), 'model', 'titanic.pkl')
+script_directory = os.path.dirname(os.path.abspath(__file__))
+pickle_file_path = os.path.join(script_directory, 'model', 'titanic.pkl')
+
 with open(pickle_file_path, 'rb') as pickle_file:
     model = pickle.load(pickle_file)
-    
+
 # Make prediction
 prediction = model.predict(input_df)
 prediction = int(prediction[0])
