61756b15cbfed096b59358addf83330074209b1c
Mohamed Hamiche <mohamed.hamiche@mydatamodels.com>
2023-08-17 11:16:34 +0200

    update model

--- titanic-classification-web-app/model/train.py
+++ titanic-classification-web-app/model/train.py
@@ -34,7 +34,7 @@ model = Pipeline(
 )
 
 # Split data 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # train the model 
 model.fit(X_train, y_train)
